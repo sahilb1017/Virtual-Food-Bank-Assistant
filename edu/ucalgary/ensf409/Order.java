de0da6aa81586109d7d2145b7a1c80880cbfe2eb
Sahil Bhatt <sahilb1017@gmail.com>
2022-04-15 01:44:30 -0600

    bug fixes

--- edu/ucalgary/ensf409/Order.java
+++ edu/ucalgary/ensf409/Order.java
@@ -9,93 +9,93 @@ public class Order {
 
     public Order(){
         //READ ALL FOODS FROM DATABASE AND STORE INTO availableFood;
-        Food cock1 = new Food(8, "food1");
-        cock1.setWholeGrain(250);
-        cock1.setFruitsVeggies( 250);
-        cock1.setProtein(250);
-        cock1.setother(250);
-        cock1.setCalories(1000);
+        Food food1 = new Food(8, "food1");
+        food1.setWholeGrain(250);
+        food1.setFruitsVeggies( 250);
+        food1.setProtein(250);
+        food1.setother(250);
+        food1.setCalories(1000);
 
 
-    Food cock2 = new Food( 12, "food2");
-        cock2.setWholeGrain(70);
-        cock2.setFruitsVeggies( 170);
-        cock2.setProtein(130);
-        cock2.setother(90);
-        cock2.setCalories(460);
+    Food food2 = new Food( 12, "food2");
+        food2.setWholeGrain(70);
+        food2.setFruitsVeggies( 170);
+        food2.setProtein(130);
+        food2.setother(90);
+        food2.setCalories(460);
 
 
-    Food cock3 = new Food(34,"food3");
-        cock3.setWholeGrain(400);
-        cock3.setFruitsVeggies(107);
-        cock3.setProtein(150);
-        cock3.setother(44);
-        cock3.setCalories(701);
+    Food food3 = new Food(34,"food3");
+        food3.setWholeGrain(400);
+        food3.setFruitsVeggies(107);
+        food3.setProtein(150);
+        food3.setother(44);
+        food3.setCalories(701);
 
 
-    Food cock4 = new Food(23, "food4");
-        cock4.setWholeGrain(200);
-        cock4.setFruitsVeggies(0);
-        cock4.setProtein(123);
-        cock4.setother(234);
-        cock4.setCalories(557);
+    Food food4 = new Food(23, "food4");
+        food4.setWholeGrain(200);
+        food4.setFruitsVeggies(0);
+        food4.setProtein(123);
+        food4.setother(234);
+        food4.setCalories(557);
 
 
-    Food cock5 = new Food(56,"food5");
-        cock5.setWholeGrain(10);
-        cock5.setFruitsVeggies(230);
-        cock5.setProtein(213);
-        cock5.setother(0);
-        cock5.setCalories(443);
+    Food food5 = new Food(56,"food5");
+        food5.setWholeGrain(10);
+        food5.setFruitsVeggies(230);
+        food5.setProtein(213);
+        food5.setother(0);
+        food5.setCalories(443);
 
-    Food cock6 = new Food(87,"food6");
-        cock6.setWholeGrain( 100);
-        cock6.setFruitsVeggies(400);
-        cock6.setProtein( 340);
-        cock6.setother( 630);
-        cock6.setCalories( 1470);
-
-
-    Food cock7 = new Food( 56,  "food7");
-        cock7.setWholeGrain( 380);
-        cock7.setFruitsVeggies( 106);
-        cock7.setProtein( 200);
-        cock7.setother(300);
-        cock7.setCalories( 986);
+    Food food6 = new Food(87,"food6");
+        food6.setWholeGrain( 100);
+        food6.setFruitsVeggies(400);
+        food6.setProtein( 340);
+        food6.setother( 630);
+        food6.setCalories( 1470);
 
 
-    Food cock8 = new Food(3,"food8");
-        cock8.setWholeGrain( 1700);
-        cock8.setFruitsVeggies(4000);
-        cock8.setProtein(8300);
-        cock8.setother(2400);
-        cock8.setCalories(16400);
-
-    Food cock9 = new Food(57, "food9");
-        cock9.setWholeGrain( 75);
-        cock9.setFruitsVeggies( 33);
-        cock9.setProtein( 78);
-        cock9.setother(6);
-        cock9.setCalories( 192);
+    Food food7 = new Food( 56,  "food7");
+        food7.setWholeGrain( 380);
+        food7.setFruitsVeggies( 106);
+        food7.setProtein( 200);
+        food7.setother(300);
+        food7.setCalories( 986);
 
 
-    Food cock10 = new Food( 64, "food10");
-        cock10.setWholeGrain( 44);
-        cock10.setFruitsVeggies( 91);
-        cock10.setProtein( 83);
-        cock10.setother(54);
-        cock10.setCalories( 272);
+    Food food8 = new Food(3,"food8");
+        food8.setWholeGrain( 1700);
+        food8.setFruitsVeggies(4000);
+        food8.setProtein(8300);
+        food8.setother(2400);
+        food8.setCalories(16400);
 
-        availableFood.add(cock1);
-        availableFood.add(cock2);
-        availableFood.add(cock3);
-        availableFood.add(cock4);
-        availableFood.add(cock5);
-        availableFood.add(cock6);
-        availableFood.add(cock7);
-        availableFood.add(cock8);
-        availableFood.add(cock9);
-        availableFood.add(cock10);
+    Food food9 = new Food(57, "food9");
+        food9.setWholeGrain( 75);
+        food9.setFruitsVeggies( 33);
+        food9.setProtein( 78);
+        food9.setother(6);
+        food9.setCalories( 192);
+
+
+    Food food10 = new Food( 64, "food10");
+        food10.setWholeGrain( 44);
+        food10.setFruitsVeggies( 91);
+        food10.setProtein( 83);
+        food10.setother(54);
+        food10.setCalories( 272);
+
+        availableFood.add(food1);
+        availableFood.add(food2);
+        availableFood.add(food3);
+        availableFood.add(food4);
+        availableFood.add(food5);
+        availableFood.add(food6);
+        availableFood.add(food7);
+        availableFood.add(food8);
+        availableFood.add(food9);
+        availableFood.add(food10);
     }
 
     public void addHamper(int[] list) throws ItemNotFoundException, NotEnoughFoodException{
